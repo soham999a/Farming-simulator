ba3ebf92bb692db2002971d7f81c1a22a95318cb
Soham Das <dassoham345@gmail.com>
2025-06-29 23:24:11 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdSmECM1DqzV1BzYIPk-6KzTjFFZEJc5A",
-  authDomain: "seed-project-a1aaf.firebaseapp.com",
-  projectId: "seed-project-a1aaf",
-  storageBucket: "seed-project-a1aaf.firebasestorage.app",
-  messagingSenderId: "28346216296",
-  appId: "1:28346216296:web:975f72033bf2e7b0f970dd",
-  measurementId: "G-DSLN3S2S9H"
+
 };
 
 const app = initializeApp(firebaseConfig);
